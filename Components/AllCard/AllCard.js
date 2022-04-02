2e0203579d54aa43719e71207d7394112f47c04c
nasirudd-in <nasir.diudevs@gmail.com>
2022-04-02 12:22:18 +0600

    filter added

--- Components/AllCard/AllCard.js
+++ Components/AllCard/AllCard.js
@@ -6,20 +6,73 @@ import Typography from '@mui/material/Typography';
 import { Box, Button, CardActionArea, CardActions, Container, Grid } from '@mui/material';
 import { useState, useEffect } from 'react';
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+
+const names = [
+      'Toyota',
+      'Nissan',
+      'HondaHonda',
+      'Hyundai',
+];
 export default function AllCard() {
 
     const [car, setCar] = useState({});
+
+    // useEffect(() => {
+    //   fetch("https://backend.bhalogari.com/api/cars/choose-by-maker/?maker_name")
+    //   .then((response) => response.json())
+    //  .then((data) => setCar(data));
+      
+    //  }, []);
+
+    const [carf, setCarf] = useState({});
+
+    const handleChange = (event) => {
+      const carSelect =  event.target.value;
+      // const carChakni = car.results.filter(cars => cars.car_manufacturer.maker_name.toLowerCase().includes(carSelect.toLowerCase()));
+      setCarf(carSelect);
+      
+    };
   
-    
+    const [carAll, setCarAll] = useState({});
+
     useEffect(() => {
-       fetch("https://backend.bhalogari.com/api/cars/choose-by-maker/?format=json&maker_name")
-       .then((response) => response.json())
-      .then((data) => setCar(data));
-       
-      }, []);
+      let url = `https://backend.bhalogari.com/api/cars/choose-by-maker/?maker_name=${carf}` ;
+      fetch(url)
+      .then((response) => response.json())
+     .then((data) => setCar(data));
+      
+     }, [carf]);
+    
    
   return (
 <Container >
+<Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+        
+          onChange={handleChange}
+        >
+         {names.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+              
+            >
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+
     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {car.results?.map((data, index) => (
          
